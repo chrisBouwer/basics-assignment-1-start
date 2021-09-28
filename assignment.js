83e6561550ada1be80e7cdb07075cc5f8accc0c8
chrisBouwer <chrisbouwer01@gmail.com>
2021-09-28 11:43:00 +0200

    operators on variables

---
+++ assignment.js
@@ -0,0 +1,16 @@
+let fictionalNum = 10,
+  result;
+
+result = 18 + fictionalNum;
+console.log(result);
+
+result = fictionalNum - 3;
+console.info(result);
+result = fictionalNum * 3;
+console.info(result);
+result = fictionalNum / 2;
+console.info(result);
+
+alert(`fictionalNum = ${fictionalNum}`);
+
+alert(`result = ${result}`);
